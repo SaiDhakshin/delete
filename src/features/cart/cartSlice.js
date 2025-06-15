fba96e52c48afdab444f2235fcbd22a3d21e2849
Sai Dakshin <sai_dakshin.seshasai@bell.ca>
2025-06-15 15:32:52 +0530

    temo1

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -12,7 +12,7 @@ const cartSlice = createSlice({
             state.cart.push(action.payload);
         },
         deleteItem(state, action) {
-            state.cart = state.cart.filter(item => item.pizzaId !== action.payload.pizzaId);
+            state.cart = state.cart.filter(item => item.pizzaId !== action.payload);
         },
         increaseItemQuantity(state, action) {
             const item = state.cart.find(item => item.pizzaId === action.payload);
@@ -23,6 +23,10 @@ const cartSlice = createSlice({
             const item = state.cart.find(item => item.pizzaId === action.payload);
             item.quantity--;
             item.totalPrice = item.quantity * item.unitPrice;
+
+            if(item.quantity <= 0) {
+                cartSlice.caseReducers.deleteItem(state, action);
+            }
         },
         clearCart(state, action) {
             state.cart = [];
@@ -41,4 +45,9 @@ export const getCart = state => state.cart.cart;
 
 export function getTotalCartPrice(state) {
     return state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0)
+}
+
+export const getCurrentQuantityById = (id) => (state) =>{
+    const item = state.cart.cart.find(item => item.pizzaId === id);
+    return item ? item.quantity : 0;
 }
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,7 +2,13 @@ import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCart, getTotalCartPrice } from '../cart/cartSlice';
+import EmptyCart from '../cart/EmptyCart';
+import store from '../../store';
+import { clearCart } from '../cart/cartSlice';
+import { formatCurrency } from '../../utils/helpers';
+import { fetchAddress } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -10,39 +16,23 @@ const isValidPhone = (str) =>
     str
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
 
   const formErrors = useActionData();
-  const username = useSelector((state) => state.user.username);
+  const {username, status: addressStatus, position, address } = useSelector((state) => state.user.username);
+  const dispatch = useDispatch();
+  const isLoadingAddress = addressStatus === 'loading';
 
   // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  const [withPriority, setWithPriority] = useState(false);
+  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
+  const totalPrice = totalCartPrice + priorityPrice;
+
+  if(!cart || cart.length === 0) return <EmptyCart />
 
   return (
     <div className="px-4 py-6">
@@ -66,18 +56,25 @@ function CreateOrder() {
             )}
           </div>
         </div>
-
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
+              disabled={isLoadingAddress}
               className="input w-full"
               type="text"
               name="address"
+              defaultValue={address || ''}
               required
             />
           </div>
+          <span className='absolute right-[3px] z-50'>
+            <Button disabled={isLoadingAddress} type="small" onClick={(e) => {e.preventDefault(); dispatch(fetchAddress())}}>Get Position</Button>
+          </span>
         </div>
+
+
 
         <div className="mb-12 flex items-center gap-5">
           <input
@@ -85,8 +82,8 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority" className="font-medium">
             Want to yo give your order priority?
@@ -96,7 +93,7 @@ function CreateOrder() {
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button disabled={isSubmitting} type="primary">
-            {isSubmitting ? 'Placing order....' : 'Order now'}
+            {isSubmitting ? 'Placing order....' : `Order now from ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
       </Form>
@@ -111,7 +108,7 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === 'on',
+    priority: data.priority === 'true',
   };
 
   const errors = {};
@@ -123,11 +120,12 @@ export async function action({ request }) {
 
   // If everything is okay, create new order and redirect
 
-  // const newOrder = await createOrder(order);
+  const newOrder = await createOrder(order);
+  store.dispatch(clearCart());
 
-  // return redirect(`/order/${newOrder.id}`);
+  return redirect(`/order/${newOrder.id}`);
 
-  return null;
+  // return null;
 }
 
 export default CreateOrder;

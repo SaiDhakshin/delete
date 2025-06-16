32f069cfc8f829112cd771f1e21710d852838ec3
Saidhakshin <Saidhakshin75@gmail.com>
2025-06-17 00:02:33 +0530

    25done

--- 17-the-wild-oasis/wildo/src/App.jsx
+++ 17-the-wild-oasis/wildo/src/App.jsx
@@ -1,48 +1,37 @@
-import styled from 'styled-components'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Bookings from './pages/Bookings'
+import Cabins from './pages/Cabins'
+import Users from './pages/Users'
+import Settings from './pages/Settings'
+import Account from './pages/Account'
+import Login from './pages/Login'
+import PageNotFound from './pages/PageNotFound'
 import GlobalStyles from './styles/GlobalStyles'
-import Button from './ui/Button'
-import Input from './ui/Input'
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`
-
-const StyledApp = styled.div`
-  padding: 20px;
-`
-
-import './App.css'
-import Heading from './ui/Heading'
-import Row from './ui/Row'
+import AppLayout from './ui/AppLayout'
 
 function App() {
-
   return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <Row type="vertical">
-          <Row type="horizontal">
-            <Heading type='h1'>Welcome to Wildo</Heading>
-            <H1>The Wild Oasis</H1>
-            <Heading type='h2'>Check in and Out</Heading>
-            <Button variations='primary' size='medium'>Check in</Button>
-            <Button variations='secondary' size='small'>Check out</Button>
-          </Row>
-          <Row type="vertical">
-            <div>
-              <Heading as='h3'>Form</Heading>
-            <form>
-              <Input type="number" placeholder="Number of guests"></Input>
-              <Input type="number" placeholder="Number of guests"></Input>
-            </form>
-            </div>
-          </Row>
-        </Row>
-      </StyledApp>
-    </>
+    <div>
+      <>
+        <GlobalStyles/>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />} >
+              <Route index element={<Navigate replace to="dashboard" />} />
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="bookings" element={<Bookings />} />
+              <Route path="cabins" element={<Cabins />} />
+              <Route path="users" element={<Users />} />
+              <Route path="settings" element={<Settings />} />
+              <Route path="account" element={<Account />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    </div>
   )
 }
 

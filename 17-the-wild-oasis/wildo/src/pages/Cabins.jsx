edea9f91b6792a4b8ff45be8c81253b924dc5a59
Sai Dakshin <sai_dakshin.seshasai@bell.ca>
2025-06-24 18:41:17 +0530

    29-2C

--- 17-the-wild-oasis/wildo/src/pages/Cabins.jsx
+++ 17-the-wild-oasis/wildo/src/pages/Cabins.jsx
@@ -2,6 +2,7 @@ import CabinsTable from "../features/cabins/CabinTable";
 import AddCabin from "../features/cabins/AddCabin";
 import Row from "../ui/Row";
 import Heading from "../ui/Heading";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
 
@@ -10,6 +11,7 @@ function Cabins() {
         <Row type="horizontal">
             <Heading as="h1">All cabins</Heading>
             <p>Filter / sort</p>
+            <CabinTableOperations />
         </Row>
         <CabinsTable />
         <AddCabin />
